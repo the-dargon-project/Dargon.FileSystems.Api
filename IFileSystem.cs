b5e94a8c9be7bf2a309d743654084980475e3a2a
Michael Yu <ItzWarty@gmail.com>
2014-11-26 15:45:15 -0800

    IFileSystem has GetName method.

--- IFileSystem.cs
+++ IFileSystem.cs
@@ -9,6 +9,7 @@ namespace Dargon.FileSystem
       IoResult ReadAllBytes(IFileSystemHandle handle, out byte[] bytes);
       void FreeHandle(IFileSystemHandle handle);
       void FreeHandles(IEnumerable<IFileSystemHandle> handles);
+      IoResult GetName(IFileSystemHandle handle, out string name);
       IoResult GetPath(IFileSystemHandle handle, out string path);
    }
 }
